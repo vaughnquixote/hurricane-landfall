d7778b6362fb001ef5d456b8f3a3d084e67a4eae
vaughnquixote <vaughnfranz@gmail.com>
2022-12-18 13:57:04 -0500

    updated data structures which store parsed data for better organization.
    added data to hold an obeservation data record.
    
    made use of shapely point class and python standard library datetime class for the geographic coordinate and the date + time of the observation respectively.
    
    updated naming conventions in variables/classes to use cyclone over hurricane where applicable

--- hurdat2_parser.py
+++ hurdat2_parser.py
@@ -1,29 +1,46 @@
 import csv
 import json
 import sys
+
 from shapely import Point
 from shapely import from_geojson
+from datetime import datetime
 
-class Hurricane:
 
-    def __init__(self, name, basin, cyclone_num, year):
+class Cyclone:
+
+    def __init__(self, name, basin, cyclone_num, year, num_observations):
         self.name = name
         self.basin = basin
         self.cyclone_num = cyclone_num
         self.year = year
         self.locations = []
         self.landfall = False
+        self.num_observations = num_observations
+        self.observations = []
+
+    def add_observation(self, observation_data):
+        self.observations.append(observation_data)
 
     def add_location(self, latitude, longitude):
         new_loc = Point(longitude, latitude)
         self.locations.append(new_loc)
 
-def parse_header(row):
+class CycloneObservationData:
+
+    def __init__(self, location, datetime, record_identifier, max_wind):
+        self.location = location
+        self.datetime = datetime
+        self.record_identifier = record_identifier
+        self.max_wind = max_wind
+
+def transform_header_to_cyclone(row):
     basin = row[0][:2]
     cyclone_num = row[0][2:4]
     year = row[0][4:]
     name = row[1].strip()
-    return Hurricane(name, basin, cyclone_num, year)
+    num_observations = int(row[2].strip()) 
+    return Cyclone(name, basin, cyclone_num, year, num_observations)
 
 def parse_coord_string(coordinate):
     cardinal_dir = coordinate[-1]
@@ -33,13 +50,24 @@ def parse_coord_string(coordinate):
         numeric_coordinate *= -1
     return numeric_coordinate
 
-def parse_data_row(row):
+def transform_data_row_to_cod(row):
+    year = int(row[0][:4])
+    month = int(row[0][4:6])
+    day = int(row[0][6:])
+    time = row[1].strip()
+    hours = int(time[:2])
+    minutes = int(time[2:])
+    cyclone_datetime = datetime(year, month, day, hours, minutes)
+
     indicator = row[2].strip()
-    lat = row[4].strip()
-    lon = row[5].strip()
-    lat = parse_coord_string(lat)
-    lon = parse_coord_string(lon)
-    return indicator, lat, lon
+    lat_str = row[4].strip()
+    lon_str = row[5].strip()
+    lat_parsed = parse_coord_string(lat_str)
+    lon_parsed = parse_coord_string(lon_str)
+    loc = Point(lon_parsed, lat_parsed)
+
+    max_wind_speed = row[6].strip()
+    return CycloneObservationData(loc, cyclone_datetime, indicator, max_wind_speed)
     
 def parse_hurdat2_file(filename):
     
@@ -52,17 +80,15 @@ def parse_hurdat2_file(filename):
 
         for row in reader:
             if len(row) == 4:
-                current_hurricane = parse_header(row)
+                current_hurricane = transform_header_to_cyclone(row)
                 hurricanes.append(current_hurricane)
             elif len(row) == 21:
-                indicator, lat, lon = parse_data_row(row)
-                current_hurricane.add_location(lat, lon)
-                if indicator == "L":
+                cyc_observation = transform_data_row_to_cod(row)
+                current_hurricane.add_observation(cyc_observation)
+                if cyc_observation.record_identifier == "L":
                     current_hurricane.landfall = True
-            else:
-                irregular_data.append(row)
     
-    return hurricanes, irregular_data
+    return hurricanes
 
 def get_polygon_from_geojson(geojson_file):
     
@@ -74,43 +100,43 @@ def get_polygon_from_geojson(geojson_file):
     
     return florida_polygon
 
-def identify_landfall_in_polygon(hurricane_list, polygon):
+def identify_landfall_in_polygon(cyclone_list, polygon):
     num_landfall_verified = 0
     num_landfall_fl = 0
     no_landfall = 0
     landfall_indicator_and_not_identified = 0
     no_ind_ided = 0
 
-    for hurr in hurricane_list:
+    for cyc in cyclone_list:
         has_landfall = False
-        for loc in hurr.locations:
-            if polygon.contains(loc):
+        for obs in cyc.observations:
+            if polygon.contains(obs.location):
                 has_landfall = True
                 break
 
         if has_landfall:
             num_landfall_fl += 1
-        if not (has_landfall or hurr.landfall):
+        if not (has_landfall or cyc.landfall):
             no_landfall += 1
-        if has_landfall and hurr.landfall:
+        if has_landfall and cyc.landfall:
             num_landfall_verified += 1
-        if not has_landfall and hurr.landfall:
+        if not has_landfall and cyc.landfall:
             landfall_indicator_and_not_identified += 1
-        if has_landfall and not hurr.landfall:
+        if has_landfall and not cyc.landfall:
             no_ind_ided += 1
     
     return num_landfall_fl, no_landfall, num_landfall_verified, landfall_indicator_and_not_identified, no_ind_ided
 
-def process_hurricane_data(hurdat2_file, geojson_file):
-    hurricanes = []
+def process_cyclone_data(hurdat2_file, geojson_file):
+    cyclones = []
     
-    hurricanes, _ = parse_hurdat2_file(hurdat2_file)
+    cyclones = parse_hurdat2_file(hurdat2_file)
 
-    print(f"processed {len(hurricanes)} hurricanes")
+    print(f"processed {len(cyclones)} hurricanes")
 
     florida = get_polygon_from_geojson(geojson_file)
 
-    num_landfall, no_landfall, verified, missed, potential_bad_data = identify_landfall_in_polygon(hurricanes, florida)
+    num_landfall, no_landfall, verified, missed, potential_bad_data = identify_landfall_in_polygon(cyclones, florida)
     
     print(f"identified landfall in florida: {num_landfall}")
     print(f"verified landfall: {verified}")
@@ -122,7 +148,7 @@ def process_hurricane_data(hurdat2_file, geojson_file):
 def main():
     hurdat2_file = sys.argv[1]
     geojson_file = sys.argv[2]
-    process_hurricane_data(hurdat2_file, geojson_file)
+    process_cyclone_data(hurdat2_file, geojson_file)
 
 if __name__ == "__main__":
     main()
