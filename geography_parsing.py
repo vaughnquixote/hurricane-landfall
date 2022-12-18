b3f7a4a0537de906299d086943c47c2087007926
vaughnquixote <vaughnfranz@gmail.com>
2022-12-18 18:26:24 -0500

    removed extraneous code and print statements

--- geography_parsing.py
+++ geography_parsing.py
@@ -8,9 +8,6 @@ def get_polygon_from_geojson(geojson_file):
     
     with open(geojson_file) as file:
         state_borders = json.load(file)
-        # for feat in state_borders["features"]:
-        #     if feat["properties"]["NAME"] == "Florida":
-        #         polygon = from_geojson(json.dumps(feat))
         polygon = from_geojson(json.dumps(state_borders))
     
     return polygon
@@ -20,6 +17,5 @@ def get_polygon_from_shapefile(shapefile_name):
     sf = shapefile.Reader(shapefile_name)
 
     polygon = from_geojson(json.dumps(sf.__geo_interface__))
-    print(len(sf.__geo_interface__["features"][0]["geometry"]["coordinates"]))
 
     return polygon
